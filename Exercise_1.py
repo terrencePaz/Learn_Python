cd01331b79b2560555d00da86e6e059fe7d53157
terrencePaz <terrence.paz@yahoo.com>
2021-12-05 09:02:37 -0800

    Exercise 1 completed

---
+++ Exercise_1.py
@@ -0,0 +1,13 @@
+# Terrence Paz
+# Learn Python 3 the Hard Way
+# Exercise 1
+# December 4, 2021
+
+print("Hello World")
+print("Hello Again")
+print("I like typing like this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do no touch this.')
+
